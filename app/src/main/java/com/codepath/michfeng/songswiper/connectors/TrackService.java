d87a9dd5ce692abf22a5e83d6870448e5ef70f75
Michelle Feng <michfeng@fb.com>
2022-06-28 10:45:29 -0700

    Created classes to store/get track data

---
+++ app/src/main/java/com/codepath/michfeng/songswiper/connectors/TrackService.java
@@ -0,0 +1,10 @@
+package com.codepath.michfeng.songswiper.connectors;
+
+import com.codepath.michfeng.songswiper.models.Track;
+
+import java.util.ArrayList;
+
+public class TrackService {
+    private ArrayList<Track> songs;
+
+}
